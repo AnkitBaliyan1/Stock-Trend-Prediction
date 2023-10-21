4a8af360df905bd75f578e5e3dc0ae5e019ff1ae
Ankit Baliyan <a.baliyan008@gmail.com>
2023-10-21 10:40:22 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -90,10 +90,10 @@ y_test=np.array(y_test)
 
 st.write("Loading model now")
 # load my model
-#model = joblib.load('model/keras_model.pkl')
-pickle_in = open('model/keras_model.pkl','rb')
-with open('model/keras_model.pkl', 'rb') as file:
-    model = joblib.load(file)
+model = joblib.load('model/keras_model.pkl')
+#pickle_in = open('model/keras_model.pkl','rb')
+#with open('model/keras_model.pkl', 'rb') as file:
+#    model = joblib.load(file)
 
 #model =load_model('model/keras_model.h5')
 st.write("model loaded")
